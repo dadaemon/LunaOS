596f62db38cf28d63bb30f260f6216df3e380b07
Luna_Morte <bolusdebeer@gmail.com>
2015-10-15 12:33:40 +0200

    Downloading added

--- LunaOS.lua
+++ LunaOS.lua
@@ -1,6 +1,57 @@
+function downloadFile(web,local)
+  print("Updating '" .. local .. "'")
+  
+  request = http.get(web)
+  data = request.readAll()
+  
+  if fs.exists(local) then
+    fs.delete(local)
+    file = fs.open(local, "w")
+    file.write(data)
+    file.close()
+  else
+    file = fs.open(local, "w")
+    file.write(data)
+    file.close()
+  end
+  
+  print("Update complete!")
+end
+
+
 term.clear()
 term.setCursorPos(1,1)
-print("LunaOS starting up...")
+local clientVersion = "0.1"
+print("LunaOS " .. clientVersion .. " starting up...")
+
+-- Check if there is an update
+versionRequest = http.get("https://raw.githubusercontent.com/dadaemon/LunaOS/master/LunaOS.ver")
+serverVersion = versionRequest.readAll()
+
+if serverVersion ~= clientVersion then
+  print("Version "..serverVersion.." is now available. Updating!")
+
+  -- lib folder
+  if fs.isDir("./LunaOS/lib") == false then
+    fs.makeDir("./LunaOS/lib")
+  end
+  -- lib/funcs
+  downloadFile("https://raw.githubusercontent.com/dadaemon/LunaOS/master/lib/funcs", "./LunaOS/libs/funcs")
+  -- lib/button
+  downloadFile("https://raw.githubusercontent.com/dadaemon/LunaOS/master/lib/button", "./LunaOS/libs/button")
+  
+  -- cnf folder
+  if fs.isDir("./LunaOS/cnf") == false then
+    fs.makeDir("./LunaOS/cnf")
+  end
+  
+  -- LunaOS.lua
+  downloadFile("https://raw.githubusercontent.com/dadaemon/LunaOS/master/LunaOS.lua", "./LunaOS/LunaOS.lua")
+  
+  print("Rebooting computer...")
+  sleep(2)
+  os.reboot()
+end
 
 -- Load files
 --os.loadAPI("./LunaOS/libs/touchpoint")
